9c031cccabd9d041e3784196d97789af21955636
Olga Yurina <olga.nik.yurina@gmail.com>
2023-05-20 13:22:01 +0300

    Change conditional commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
-    val likes = 61
+    val likes = 1111
     val likesLastDigit = likes%10
-    val variantEnding = if (likesLastDigit == 1) " человеку" else " людям"
+    val likesSecondOrderDigit =  (likes/10)%10
+    val variantEnding = if (likesLastDigit == 1 && likesSecondOrderDigit !=1) " человеку" else " людям"
     println("Понравилось " + likes + variantEnding )
-}
+  }
